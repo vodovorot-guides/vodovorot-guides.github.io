1fb987e36e87044a235f44695313439573517389
AmaniZandalari <72340750+AmaniZandalari@users.noreply.github.com>
2022-12-13 15:23:55 +0100

    _ changed to -

--- assets/js/rotation_switches2.js
+++ assets/js/rotation_switches2.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
   $('.rotation_switches input[type="checkbox"]').change(function() {
     name = $(this).parent().attr("id").replace("rotation_switch_", "");
     if ($(this).is(':checked')) {
-      $('.rotation_line_' + name + '_off').hide();
-      $('.rotation_line_' + name + '_on').show();
+      $('.rotation_line-' + name + '-off').hide();
+      $('.rotation_line-' + name + '-on').show();
     } else { 
-      $('.rotation_line_' + name + '_on').hide();
-      $('.rotation_line_' + name + '_off').show();
+      $('.rotation_line-' + name + '-on').hide();
+      $('.rotation_line-' + name + '-off').show();
     }
   });
   $('.rotation_switches input[type="checkbox"]').prop("disabled", false);
